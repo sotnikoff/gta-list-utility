8eb5612ffe5c8573527bd60286e092dca1e63288
Mikhail Sotnikov <thehofch@gmail.com>
2020-03-26 14:07:35 +0300

    fixes

--- player_logs/parser.rb
+++ player_logs/parser.rb
@@ -20,12 +20,12 @@ module PlayerLogs
 
     def read_file(file)
       f = File.open(file).read
-      matches = f.match(/Name=([a-zA-Z0-9_-]*)\r\nIP=(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\r\nRockstar ID=(\d*)/)
+      matches = f.match(/Name=([.a-zA-Z0-9_-]*)\r\nIP=(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\r\nRockstar ID=(\d*)/)
       { r_star_id: matches[3], name: matches[1], ip: matches[2] }.compact if check_match(matches)
     end
 
     def check_match(matches)
-      !(matches[1].nil? || matches[2].nil? || matches[3].nil?)
+      !matches.nil? && !(matches[1].nil? || matches[2].nil? || matches[3].nil?)
     end
   end
 end
--- start.rb
+++ start.rb
@@ -3,7 +3,7 @@ require_relative 'mamken_schutken/sender.rb'
 require_relative 'player_logs/parser.rb'
 require_relative 'player_logs/sender.rb'
 
-token = 'eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoxLCJuaWNrbmFtZSI6ImhvZmNoIn0sImV4cCI6MTU4NTE0NzcwOX0.PL-xnGseZSDUN3nIxsLMOyDuvLQUKo-iFQODLw1E8to'
+token = 'eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoxLCJuaWNrbmFtZSI6ImhvZmNoIn0sImV4cCI6MTYxNTg0ODk1MX0.xP3O_mXUaROQaEARsLGEVoWbAS5CEpkZQnh4-PG_CiI'
 address = 'localhost:3000'
 
 PlayerLogs::Parser.parse(token, address)
